f8fe560242e9d05cd307c808e9a2e223ca0d6a34
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-08-06 14:22:44 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# /opt/hokage-bot/config.py (VERSI FINAL YANG BENAR)
+
+import os
+
+# Membaca variabel dari file .env
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+KMSP_API_KEY = os.getenv("KMSP_API_KEY")
+
+# ID Admin bisa diatur di sini atau di .env
+ADMIN_IDS = {1469244768} # Ganti dengan ID admin Anda
